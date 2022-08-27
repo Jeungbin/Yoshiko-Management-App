123852f26ad687d817bb91ebec4cf14048d2f79c
jeongbin <qls2467@gmail.com>
2022-08-26 22:05:13 -0600

    Edit Minor Style

--- crud_app/assets/js/main.js
+++ crud_app/assets/js/main.js
@@ -8,8 +8,19 @@ const serchUser = document.getElementById("serchUser");
 let classDateOption = document.getElementById("class-date");
 let teacherOption = document.getElementById("teacher");
 const remainingDateInput = document.getElementById("remaining-date-input");
+const totalUser = document.getElementById("total-user");
 
 let parsedData;
+let dDayResult;
+function remainingDateCal(dateString) {
+  const startDate = dateString;
+  const year = parseInt(startDate.slice(0, 4));
+  const month = parseInt(startDate.slice(5, 7));
+  const day = parseInt(startDate.slice(8, 10));
+  let date = new Date(year, month - 1, day);
+  date.setDate(date.getDate() + 28);
+  return date.toLocaleString("ko-KR", { timeZone: "UTC" });
+}
 
 function displayAllusers(data) {
   try {
@@ -22,61 +33,85 @@ function displayAllusers(data) {
   overDateTable.innerHTML = "";
   userTable.innerHTML = "";
   userTableLessSeven.innerHTML = "";
-
+  totalUser.innerHTML = parsedData.length;
+  // console.log(parsedData.startDate.slice(0, 10));
   parsedData.map((item) => {
-    // if (item.remainingDate <= 0) {
-    //   return (overDateTable.innerHTML =
-    //     overDateTable.innerHTML +
-    //     `
-    //     <tr class='box' id=${item.remainingDate}>
-    //     <td id="name">${item.name}</td>
-    //     <td>${item.phoneNumber}</td>
-    //     <td>${item.classDate}</td>
-    //     <td>${item.startingDate}</td>
-    //     <td style="background-color:red;">${item.remainingDate}</td>
-    //     <td>${item.teacher}</td>
-    //     <td>
-    //         <a href="#" class="btn border-shadow update">
-    //             <span class="text-gradient"><i class="fas fa-pencil-alt"></i></span>
-    //         </a>
-    //         <a class="btn border-shadow delete">
-    //             <span class="text-gradient"><i class="fas fa-times"></i></span>
-    //         </a>
-    //     </td>
-    //     </tr>
-    //       `);
-    // }
-    // if (item.remainingDate <= 7) {
-    //   return (userTableLessSeven.innerHTML =
-    //     userTableLessSeven.innerHTML +
-    //     `
-    //     <tr class='box' id=${item.remainingDate}>
-    //     <td id="name">${item.name}</td>
-    //     <td>${item.phoneNumber}</td>
-    //     <td>${item.classDate}</td>
-    //     <td>${item.startingDate}</td>
-    //     <td style="background-color:grey;">${item.remainingDate}</td>
-    //     <td>${item.teacher}</td>
-    //     <td>
-    //         <a href="#" class="btn border-shadow update">
-    //             <span class="text-gradient"><i class="fas fa-pencil-alt"></i></span>
-    //         </a>
-    //         <a class="btn border-shadow delete">
-    //             <span class="text-gradient"><i class="fas fa-times"></i></span>
-    //         </a>
-    //     </td>
-    //     </tr>
-    //       `);
-    // } else {
-    return (userTable.innerHTML =
-      userTable.innerHTML +
-      `
+    const lastDate = item.startDate ? remainingDateCal(item.startDate) : "";
+    console.log(lastDate);
+    const lastdateYear = parseInt(lastDate.slice(0, 4));
+    const lastdateMonth = parseInt(lastDate.slice(5, 7));
+    const lastdateDate = parseInt(lastDate.slice(9, 11));
+
+    const today = new Date();
+    const dday = new Date(lastdateYear, lastdateMonth - 1, lastdateDate);
+    const gap = dday.getTime() - today.getTime();
+    var dDayResult = Math.ceil(gap / (1000 * 60 * 60 * 24));
+    console.log(dDayResult);
+    if (dDayResult <= 0) {
+      return (overDateTable.innerHTML =
+        overDateTable.innerHTML +
+        `
+        <tr class='box' id=${item.remainingDate}>
+        <td id="name">${item.name}</td>
+        <td>${item.phoneNum}</td>
+        <td>${item.classDate}</td>
+        <td>${
+          item.startDate
+            ? `${item.startDate.slice(5, 7)} / ${item.startDate.slice(8, 10)}`
+            : ""
+        }</td>
+        <td style="background-color:red; border: 1px solid;">${dDayResult}</td>
+        <td>${item.teacher}</td>
+        <td>
+            <a href="#" class="btn border-shadow update">
+                <span class="text-gradient"><i class="fas fa-pencil-alt"></i></span>
+            </a>
+            <a class="btn border-shadow delete">
+                <span class="text-gradient"><i class="fas fa-times"></i></span>
+            </a>
+        </td>
+        </tr>
+          `);
+    }
+    if (dDayResult > 0 && dDayResult <= 7) {
+      return (userTableLessSeven.innerHTML =
+        userTableLessSeven.innerHTML +
+        `
+        <tr class='box' id=${item.remainingDate}>
+        <td id="name">${item.name}</td>
+        <td>${item.phoneNum}</td>
+        <td>${item.classDate}</td>
+        <td>${
+          item.startDate
+            ? `${item.startDate.slice(5, 7)} / ${item.startDate.slice(8, 10)}`
+            : ""
+        }</td>
+        <td style="background-color:grey; border: 1px solid;">${dDayResult}</td>
+        <td>${item.teacher}</td>
+        <td>
+            <a href="#" class="btn border-shadow update">
+                <span class="text-gradient"><i class="fas fa-pencil-alt"></i></span>
+            </a>
+            <a class="btn border-shadow delete">
+                <span class="text-gradient"><i class="fas fa-times"></i></span>
+            </a>
+        </td>
+        </tr>
+          `);
+    } else {
+      return (userTable.innerHTML =
+        userTable.innerHTML +
+        `
       <tr class='box' id=${item.remainingDate}>
       <td id="name">${item.name}</td>
       <td>${item.phoneNum}</td>
       <td>${item.classDate}</td>
-      <td>${item.startDate}</td>
-      <td id='remainingDate' + ${item.id}>${item.remainingDate}</td>
+      <td>${
+        item.startDate
+          ? `${item.startDate.slice(5, 7)} / ${item.startDate.slice(8, 10)}`
+          : ""
+      }</td>
+      <td id='remainingDate' + ${item.id}>${dDayResult}</td>
       <td>${item.teacher}</td>
       <td>
           <a href="#" class="btn border-shadow update">
@@ -88,7 +123,8 @@ function displayAllusers(data) {
       </td>
       </tr>
         `);
-    // }
+      // }
+    }
   });
 }
 
@@ -118,11 +154,14 @@ function liveSearchName(e) {
 
 function liveSearchDate() {
   let serch_query = parseInt(remainingDateInput.value);
+  console.log(serch_query);
+
   for (var i = 0; i < tables.length; i++) {
+    console.log(dDayResult);
     if (serch_query === "") {
       tables[i].classList.remove("is-hidden");
     } else {
-      if (tables[i].id <= serch_query) {
+      if (dDayResult <= serch_query) {
         tables[i].classList.remove("is-hidden");
       } else {
         tables[i].classList.add("is-hidden");
@@ -131,28 +170,28 @@ function liveSearchDate() {
   }
 }
 
-resetBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  for (var i = 0; i < parsedData.length; i++) {
-    tables[i].classList.remove("is-hidden");
-  }
-  document.getElementById("searchBar").value = "";
-});
+// resetBtn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   for (var i = 0; i < parsedData.length; i++) {
+//     tables[i].classList.remove("is-hidden");
+//   }
+//   document.getElementById("searchBar").value = "";
+// });
 
-resetBtn2.addEventListener("click", (e) => {
-  e.preventDefault();
-  for (var i = 0; i < tables.length; i++) {
-    tables[i].classList.remove("is-hidden");
-  }
-  document.getElementById("remaining-date-input").value = "";
-});
+// resetBtn2.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   for (var i = 0; i < tables.length; i++) {
+//     tables[i].classList.remove("is-hidden");
+//   }
+//   document.getElementById("remaining-date-input").value = "";
+// });
 
 function filter() {
   let serch_query_classDate =
     classDateOption.options[classDateOption.selectedIndex].text;
   let serch_query_teacher =
     teacherOption.options[teacherOption.selectedIndex].text;
-  console.log(parsedData[0].classDate);
+  //console.log(serch_query_classDate);
   if (
     serch_query_classDate === "Class Date" &&
     serch_query_teacher === "Teacher"
@@ -166,6 +205,7 @@ function filter() {
     serch_query_teacher === "Teacher"
   ) {
     for (var i = 0; i < parsedData.length; i++) {
+      console.log(parsedData[2].classDate);
       if (parsedData[i].classDate === serch_query_classDate) {
         tables[i].classList.remove("is-hidden");
       } else {
@@ -208,8 +248,8 @@ showAllUserBtn.addEventListener("click", (e) => {
   for (var i = 0; i < parsedData.length; i++) {
     tables[i].classList.remove("is-hidden");
   }
-  classDateOption.value = "class-date";
-  teacherOption.value = "teacher";
+  document.getElementById("searchBar").value = "";
+  document.getElementById("remaining-date-input").value = "";
 });
 
 let typingTimer;
